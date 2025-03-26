a63ce80c19d0e9ace633160ef17d4ad8d8d2b79c
angel Perez <78099130+angel-perez213@users.noreply.github.com>
2025-03-26 12:41:58 -0600

    parcial 1 resuelto

--- src/main/java/ed/lab/E01FlattenBT.java
+++ src/main/java/ed/lab/E01FlattenBT.java
@@ -2,8 +2,16 @@ package ed.lab;
 
 public class E01FlattenBT {
 
+    private TreeNode<Integer> prev = null;
+
     public void flatten(TreeNode<Integer> root) {
+        if (root == null) return;
 
+        flatten(root.right);
+        flatten(root.left);
+
+        root.right = prev;
+        root.left = null;
+        prev = root;
     }
-
 }
